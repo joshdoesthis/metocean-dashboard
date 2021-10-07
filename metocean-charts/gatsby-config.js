cb2a1796d6e82d9c04f3249ae7c8651bcad08b3b
Josh Wilson <joshdoesthis@icloud.com>
2021-10-08 12:43:11 +1300

    Delete gatsby-config.js

--- metocean-charts/gatsby-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "Metocean Charts",
-  },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-react-helmet"],
-};
