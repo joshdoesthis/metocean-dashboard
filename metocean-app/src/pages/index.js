f85fb999a9531b4cae5af17cec88f03a81d0c8cd
Josh Wilson <joshdoesthis@icloud.com>
2021-10-09 11:11:15 +1300

    Update index.js

--- metocean-app/src/pages/index.js
+++ metocean-app/src/pages/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import * as D3 from 'd3';
+import * as V from 'victory';
 
 // Styles
 const pageStyles = {};
@@ -8,7 +9,7 @@ const pageStyles = {};
 // Markup
 const IndexPage = () => {
   // Client-side Runtime Data Fetching
-  const [metoceanData, setMetoceanData] = useState(0);
+  const [metoceanData, setMetoceanData] = useState([]);
 
   useEffect(() => {
     axios({
@@ -19,11 +20,7 @@ const IndexPage = () => {
           query Query {
             metoceanAll {
               time
-              sst
-              vis
-              rh
-              tmp
-              precip
+              wsp
             }
           }
       `,
@@ -31,7 +28,24 @@ const IndexPage = () => {
     }).then((result) => setMetoceanData(result.data.data));
   }, []);
 
-  return <main style={pageStyles}></main>;
+  return (
+    <main style={pageStyles}>
+      <V.VictoryChart
+        domainPadding={10}
+        scale={{
+          x: D3.scaleUtc(),
+          y: 'linear',
+        }}
+      >
+        <V.VictoryBar
+          data={metoceanData.metoceanAll}
+          x='time'
+          y='wsp'
+          style={{ data: { fill: 'white' } }}
+        />
+      </V.VictoryChart>
+    </main>
+  );
 };
 
 export default IndexPage;
