6f875cf17d274c2c193870d49e03ffc6a4f50be6
Josh Wilson <joshdoesthis@icloud.com>
2021-10-11 00:31:05 +1300

    Update index.js

--- metocean-app/src/pages/index.js
+++ metocean-app/src/pages/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Wrapper from '../components/Layout';
+import { Wrapper } from '../components/Layout';
+import Legend from '../components/Legend';
 import MetoceanAllQuery from '../graphql/MetoceanAllQuery';
 import WindSpeedLines from '../components/WindSpeedLines';
 
@@ -25,13 +26,27 @@ const IndexPage = () => {
     <Wrapper>
       {metoceanData.metoceanAll && (
         <>
+          <Legend
+            title={'Wind Speed'}
+            unit={'Knots'}
+            labels={[
+              { short: 'gst', name: 'Typical Gust', color: '#FFF59D' },
+              { short: 'wsp', name: '10m', color: '#F4511E' },
+              { short: 'wsp50', name: '50m', color: '#DCE775' },
+              { short: 'wsp80', name: '80m', color: '#8BC34A' },
+              { short: 'wsp100', name: '100m', color: '#00796B' },
+            ]}
+          />
           <WindSpeedLines
+            height={100}
             metoceanData={metoceanData.metoceanAll}
             selectedDomain={selectedDomain}
             setSelectedDomain={setSelectedDomain}
             withZoom
+            withAxis
           />
           <WindSpeedLines
+            height={25}
             metoceanData={metoceanData.metoceanAll}
             selectedDomain={selectedDomain}
             setSelectedDomain={setSelectedDomain}
