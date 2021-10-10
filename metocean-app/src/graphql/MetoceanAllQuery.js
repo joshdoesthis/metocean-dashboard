ea4223c688b76a5dac4065093844e89bf0468fb4
Josh Wilson <joshdoesthis@icloud.com>
2021-10-10 13:36:01 +1300

    Create MetoceanAllQuery.js

---
+++ metocean-app/src/graphql/MetoceanAllQuery.js
@@ -0,0 +1,48 @@
+const MetoceanAllQuery = `
+  query Query {
+    metoceanAll {
+      time
+      lev
+      hs
+      hx
+      tp
+      tm01
+      tm02
+      dp
+      dpm
+      hs_sw1
+      hs_sw8
+      tp_sw1
+      tp_sw8
+      dpm_sw8
+      dpm_sw1
+      hs_sea8
+      hs_sea
+      tp_sea8
+      tp_sea
+      tm_sea
+      dpm_sea8
+      dpm_sea
+      hs_ig
+      hs_fig
+      wsp
+      gst
+      wd
+      wsp100
+      wsp50
+      wsp80
+      precip
+      tmp
+      rh
+      vis
+      cld
+      cb
+      csp0
+      cd0
+      ss
+      sst
+    }
+  }
+`;
+
+export default MetoceanAllQuery;
