73f96a518dacfe55c3a50979f999096ed17e53fc
Josh Wilson <joshdoesthis@icloud.com>
2021-10-10 15:48:23 +1300

    Update Layout.js

--- metocean-app/src/components/Layout.js
+++ metocean-app/src/components/Layout.js
@@ -1,20 +1,14 @@
 import React from 'react';
 
-const styles = { sideBar: {}, ContentWrapper: {}, wrapper: {} };
-
-export const SideBar = (props) => {
-  return <div id='sidebar'>{props.children}</div>;
+const Area = (props) => {
+  return <div>{props.children}</div>;
 };
 
-export const ContentWrapper = (props) => {
-  return <div style={styles.contentWrapper}>{props.children}</div>;
-};
+const Wrapper = (props) => (
+  <Area>
+    <Area />
+    <Area>{props.children}</Area>
+  </Area>
+);
 
-export const Wrapper = (props) => {
-  return (
-    <div id='wrapper'>
-      {<SideBar {...props} />}
-      {<ContentWrapper {...props} />}
-    </div>
-  );
-};
+export default Wrapper;
