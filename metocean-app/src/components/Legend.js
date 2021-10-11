bfba309dc5dc3329472bfca0ce82fc14b9e5cf5b
Josh Wilson <joshdoesthis@icloud.com>
2021-10-12 09:45:29 +1300

    Update Legend.js

--- metocean-app/src/components/Legend.js
+++ metocean-app/src/components/Legend.js
@@ -18,6 +18,10 @@ const styles = {
     color: '#90A4AE',
     fontSize: '3vw',
   },
+  scale: {
+    display: 'inline-block',
+    cursor: 'pointer',
+  },
   labels: {
     display: 'inline-flex',
     fontSize: '2vw',
@@ -50,10 +54,44 @@ const styles = {
 };
 
 const Legend = (props) => {
+  const { defaultUnit, selectedUnit } = props;
+
+  const currentUnit = selectedUnit.scale ? selectedUnit.scale : defaultUnit;
+
+  const scales = {
+    kts: [
+      { scale: 'kts', ratio: 1 },
+      { scale: 'mph', ratio: 1.15078 },
+      { scale: 'km/h', ratio: 1.852 },
+      { scale: 'm/s', ratio: 0.514444 },
+      { scale: 'ft/s', ratio: 1.68781 },
+    ],
+  };
+
   return (
     <div style={styles.legend}>
       <div style={styles.title}>{props.title}</div>
-      <div style={styles.unit}>{props.unit}</div>
+      <div style={styles.unit}>
+        <div
+          style={styles.scale}
+          onClick={() => {
+            const numScales = scales[defaultUnit].length;
+            const nextUnitIndex =
+              scales[defaultUnit].findIndex(
+                ({ scale }) => scale == currentUnit
+              ) + 1;
+
+            const nextSelectedUnit =
+              scales[defaultUnit][
+                nextUnitIndex < numScales ? nextUnitIndex : 0
+              ];
+
+            props.setSelectedUnit(nextSelectedUnit);
+          }}
+        >
+          {currentUnit}
+        </div>
+      </div>
       {props.labels && (
         <div style={styles.labels}>
           {props.labels.map((label, i) => (
