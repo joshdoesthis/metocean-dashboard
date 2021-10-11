a380e43956fd00e7db76331135ed5ac80b66419b
Josh Wilson <joshdoesthis@icloud.com>
2021-10-12 09:45:22 +1300

    Update WindSpeedLines.js

--- metocean-app/src/components/WindSpeedLines.js
+++ metocean-app/src/components/WindSpeedLines.js
@@ -4,20 +4,17 @@ import Group from './Group';
 import Line from './Line';
 import { Axis, DependentAxis } from './Axis';
 
-const WindSpeedLines = (props) => {
-  return (
-    <TimeScaleChart {...props}>
-      <Group>
-        {props.filter.map(
-          (el, i) =>
-            el.active &&
-            Line({ ...props, key: i, stroke: el.color, y: el.short })
-        )}
-      </Group>
-      {Axis({ ...props })}
-      {DependentAxis({ ...props })}
-    </TimeScaleChart>
-  );
-};
+const WindSpeedLines = (props) => (
+  <TimeScaleChart {...props}>
+    <Group>
+      {props.activeLines.map(
+        (el, i) =>
+          el.active && Line({ ...props, key: i, stroke: el.color, y: el.short })
+      )}
+    </Group>
+    {Axis({ ...props })}
+    {DependentAxis({ ...props })}
+  </TimeScaleChart>
+);
 
 export default WindSpeedLines;
