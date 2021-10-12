a348ff4dc161b7afd44728fd62c0764f6d283ac7
Josh Wilson <joshdoesthis@icloud.com>
2021-10-12 17:17:40 +1300

    Update Legend.js

--- metocean-app/src/components/Legend.js
+++ metocean-app/src/components/Legend.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as D3 from 'd3';
 
 const styles = {
   legend: {
@@ -53,76 +54,77 @@ const styles = {
   },
 };
 
-const Legend = (props) => {
-  const { defaultUnit, selectedUnit } = props;
+const Legend = (props) => (
+  <div style={styles.legend}>
+    <div style={styles.title}>{props.title}</div>
+    <div style={styles.unit}>
+      <div
+        style={styles.scale}
+        onClick={() => {
+          const { defaultUnit, selectedUnit } = props;
 
-  const currentUnit = selectedUnit.scale ? selectedUnit.scale : defaultUnit;
+          const currentUnit = selectedUnit.scale
+            ? selectedUnit.scale
+            : defaultUnit;
 
-  const scales = {
-    kts: [
-      { scale: 'kts', ratio: 1 },
-      { scale: 'mph', ratio: 1.15078 },
-      { scale: 'km/h', ratio: 1.852 },
-      { scale: 'm/s', ratio: 0.514444 },
-      { scale: 'ft/s', ratio: 1.68781 },
-    ],
-  };
+          const scales = {
+            kts: [
+              { scale: 'kts', ratio: 1 },
+              { scale: 'mph', ratio: 1.151 },
+              { scale: 'km/h', ratio: 1.852 },
+              { scale: 'm/s', ratio: 1 / 1.944 },
+              { scale: 'ft/s', ratio: 1.688 },
+            ],
+          };
 
-  return (
-    <div style={styles.legend}>
-      <div style={styles.title}>{props.title}</div>
-      <div style={styles.unit}>
-        <div
-          style={styles.scale}
-          onClick={() => {
-            const numScales = scales[defaultUnit].length;
-            const nextUnitIndex =
-              scales[defaultUnit].findIndex(
-                ({ scale }) => scale == currentUnit
-              ) + 1;
+          const numScales = scales[defaultUnit].length;
 
-            const nextSelectedUnit =
-              scales[defaultUnit][
-                nextUnitIndex < numScales ? nextUnitIndex : 0
-              ];
+          const nextUnitIndex =
+            scales[defaultUnit].findIndex(({ scale }) => scale == currentUnit) +
+            1;
 
-            props.setSelectedUnit(nextSelectedUnit);
-          }}
-        >
-          {currentUnit}
-        </div>
+          const nextSelectedUnit =
+            scales[defaultUnit][nextUnitIndex < numScales ? nextUnitIndex : 0];
+
+          props.setSelectedUnit(nextSelectedUnit);
+          props.setSelectedDomain([]);
+        }}
+      >
+        {props.selectedUnit.scale
+          ? props.selectedUnit.scale
+          : props.defaultUnit}
       </div>
-      {props.labels && (
-        <div style={styles.labels}>
-          {props.labels.map((label, i) => (
-            <div
-              key={i}
-              style={styles.label}
-              onClick={() =>
-                // Toggle label and presentation of data
-                props.setActive(
-                  Object.assign(
-                    [...props.labels],
-                    ([...props.labels][i].active = ![...props.labels][i].active)
-                  )
-                )
-              }
-            >
-              <div
-                style={Object.assign(
-                  { ...styles.circle },
-                  {
-                    backgroundColor: label.active ? label.color : '#ECEFF1',
-                  }
-                )}
-              ></div>
-              <div style={styles.name}>{label.name}</div>
-            </div>
-          ))}
-        </div>
-      )}
     </div>
-  );
-};
+    {props.labels && (
+      <div style={styles.labels}>
+        {props.labels.map((label, i) => (
+          <div
+            key={i}
+            style={styles.label}
+            onClick={() =>
+              // Toggle label and presentation of data
+              props.setActive(
+                Object.assign(
+                  [...props.labels],
+                  ([...props.labels][i].active = ![...props.labels][i].active)
+                )
+              )
+            }
+          >
+            <div
+              style={Object.assign(
+                { ...styles.circle },
+                {
+                  backgroundColor: label.active ? label.color : '#ECEFF1',
+                }
+              )}
+            ></div>
+            <div style={styles.name}>{label.name}</div>
+          </div>
+        ))}
+      </div>
+    )}
+  </div>
+);
 
 export default Legend;
