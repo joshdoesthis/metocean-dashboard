275a93ee845c4f57be4c1e97cfb2d51b74fdf764
Josh Wilson <joshdoesthis@icloud.com>
2021-10-13 16:17:19 +1300

    Update Legend.js

--- metocean-app/src/components/Legend.js
+++ metocean-app/src/components/Legend.js
@@ -87,6 +87,7 @@ const Legend = (props) => (
             scales[defaultUnit][nextUnitIndex < numScales ? nextUnitIndex : 0];
 
           props.setSelectedUnit(nextSelectedUnit);
+          props.setSelectedDomain();
         }}
       >
         {props.selectedUnit.scale
